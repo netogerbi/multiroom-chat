f4d52df55c7c61dc921eeba378753261b66eac1f
Savy <neto@savy.com.br>
2019-10-25 12:43:01 -0300

    sending the nickname of every person who get in

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,11 +1,11 @@
 module.exports.startChat = function (app, req, res) {
-  
+
   req.assert('nickname', 'Apelido ou nome é obrigatório').notEmpty()
-  req.assert('nickname', 'Apelido ou nome deve conter entre 3 e 15 caracteres').len(3,15)
+  req.assert('nickname', 'Apelido ou nome deve conter entre 3 e 15 caracteres').len(3, 15)
 
   const errors = req.validationErrors()
 
-  if(errors) {
+  if (errors) {
     res.render('index', { invalid: errors })
     return;
   }
@@ -14,10 +14,10 @@ module.exports.startChat = function (app, req, res) {
   const formData = req.body
 
   // retrieving global defined in set
-  app.get('socket').emit('msgToClient', { nickname: formData.nickname,  message: 'acabou de entrar...'})
+  app.get('socket').emit('msgToClient', { nickname: formData.nickname, message: 'acabou de entrar...' })
 
 
-  res.render('chat')
+  res.render('chat', { formData: formData })
 }
 
 module.exports.getChat = function (app, req, res) {
