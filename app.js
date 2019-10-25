c1739fc9b3d0f9d9fa3082ff6e9f5e352f7cba3f
Grupo5-Reculos Gerbi Neto <netogerbi2@hotmail.com>
2019-10-24 21:13:43 -0300

    Websocket disconnection

--- app.js
+++ app.js
@@ -8,7 +8,17 @@ const io = require('socket.io').listen(listener)
 
 /** CREATE WEBSOCKET CONNECTION */
 io.on('connection', function(socket) {
-  console.log('User connected');
+  console.log('User connected')
+
+  /** DISCONNECT WEBSOCKET CONNECTION */
+  socket.on('disconnect', function() {
+    console.log('User disconnected')
+  })
 })
+
+/**
+ * When the page that has the client side file that connects with server websocket, the connection is created
+ * when the page is unloaded, the connection is closed too.
+ */
 
 console.log('Port: ' + listener.address().port)
