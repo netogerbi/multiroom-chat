b47b5fe59baddda6d9644993bd8d6e69cb8ff3db
Grupo5-Reculos Gerbi Neto <netogerbi2@hotmail.com>
2019-10-24 21:04:34 -0300

    Connecting websocket

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const listener = app.listen(8080, () => {
   console.log('server running')
 });
 
-require('socket.io').listen(listener)
+const io = require('socket.io').listen(listener)
+
+/** CREATE WEBSOCKET CONNECTION */
+io.on('connection', function(socket) {
+  console.log('User connected');
+})
 
 console.log('Port: ' + listener.address().port)
